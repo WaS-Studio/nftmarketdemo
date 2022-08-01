0399a1b1cb00fcdde19aacaa5beaf041f442ceb2
Roody-WaS <roody_io@webandsundry.com>
2022-08-01 17:47:27 -0400

    navbar commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/02c2e2ff99.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
 
 export default MyApp;
