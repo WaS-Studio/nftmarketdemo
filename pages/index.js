4f7c9bb3d963a063aed4d78ec4d21403077775c1
Roody-WaS <roody_io@webandsundry.com>
2022-08-15 14:08:06 -0400

    added creator card component

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,65 @@
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
-import { Banner } from '../components';
+import { Banner, CreatorCard } from '../components';
 
 import images from '../assets';
 
-const Home = () => (
-  <div className="flex justify-center sm:px-4 p-12">
-    <div className="w-full minmd:4/5">
-      <Banner
-        name="Discover, collect, and sell extraordinary NFTs"
-        childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
-        parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
-      />
-    </div>
+const Home = () => {
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
 
-    <div>
-      <h1 className="font-poppins dark:text-white test-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0"> Best Creators
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:4/5">
+        <Banner
+          name="Discover, collect, and sell extraordinary NFTs"
+          childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+      </div>
 
-      </h1>
-    </div>
+      <div>
+        <h1 className="font-poppins dark:text-white test-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0"> Best Creators</h1>
 
-    <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet ">
-      <Image
-        src={images.snailMan}
-        obejectfit="contain"
-        width={500}
-        height={300}
-        alt="Snail Man"
-      />
+        <div className="relative flex-1 max-w-full mt-3" ref={parentRef}>
+          <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+            {[6, 7, 8, 9, 10].map((i) => (
+              <CreatorCard />
+            ))}
+          </div>
+
+        </div>
+      </div>
+
+      <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet ">
+        <Image
+          src={images.snailMan}
+          obejectfit="contain"
+          width={500}
+          height={300}
+          alt="Snail Man"
+        />
+      </div>
+      <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet">
+        <Image
+          src={images.snailMan}
+          obejectfit="contain"
+          width={500}
+          height={300}
+          alt="Snail Man"
+        />
+      </div>
+      <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet ">
+        <Image
+          src={images.snailMan}
+          obejectfit="contain"
+          width={500}
+          height={300}
+          alt="Snail Man"
+        />
+      </div>
     </div>
-    <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet">
-      <Image
-        src={images.snailMan}
-        obejectfit="contain"
-        width={500}
-        height={300}
-        alt="Snail Man"
-      />
-    </div>
-    <div className="minmd:4/5 flex ml-20 p-1 justify-evenly bg-nft-red-violet ">
-      <Image
-        src={images.snailMan}
-        obejectfit="contain"
-        width={500}
-        height={300}
-        alt="Snail Man"
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
